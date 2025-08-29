4a34dd90fec2de0e29effa1ba8075082471d8a07
Abdullah Johar <jrrabdullah23@gmail.com>
2025-08-29 14:42:45 +0600

    quiz category page created

---
+++ lib/pages/quiz_category_page.dart
@@ -0,0 +1,20 @@
+class Category {
+    final String name;
+
+    Category(this.name);
+}
+
+final List<Category> categories = [
+    Category("Allah"),
+    Category("Qur'an"),
+    Category("Seerah"),
+    Category("Prophets"),
+    Category("Companions"),
+    Category("Women in Islam"),
+    Category("Fiqh"),
+    Category("Aqeeda"),
+    Category("Islamic History"),
+    Category("Qiyamah"),
+    Category("Jahannum"),
+    Category("Jannah"),
+];
