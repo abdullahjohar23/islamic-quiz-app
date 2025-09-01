00680da7b1adfb5ae05d06763c9b9edb3dcc3283
Abdullah Johar <jrrabdullah23@gmail.com>
2025-09-01 18:46:55 +0600

    bg image added for quiz card

--- lib/pages/quiz_category_list.dart
+++ lib/pages/quiz_category_list.dart
@@ -6,16 +6,16 @@ class Category {
 }
 
 final List<Category> categories = [
-    Category("Allah", "assets/images/quizbg/Allahbg.png"),
-    Category("Qur'an", "assets/images/quizbg/quranbg.png"),
-    Category("Seerah", "assets/images/quizbg/prophetmuhammadbg.png"),
+    Category("Names of Allah", "assets/images/quizbg/Allahbg.jpg"),
+    Category("Qur'an", "assets/images/quizbg/quranbg.jpg"),
+    Category("Seerah", "assets/images/quizbg/prophetmuhammadbg.jpg"),
     Category("Prophets", "assets/images/quizbg/prophetsbg.jpg"),
     Category("Companions", "assets/images/quizbg/companionsbg.jpg"),
     Category("Women in Islam", "assets/images/quizbg/womenbg.jpg"),
     Category("Fiqh", "assets/images/quizbg/fiqhbg.png"),
     Category("Aqeeda", "assets/images/quizbg/quranbg.png"),
-    Category("Islamic History", "assets/images/quizbg/quranbg.png"),
+    Category("Battles", "assets/images/quizbg/battlebg.jpg"),
     Category("Qiyamah", "assets/images/quizbg/quranbg.png"),
-    Category("Jahannum", "assets/images/quizbg/quranbg.png"),
-    Category("Jannah", "assets/images/quizbg/quranbg.png"),
+    Category("Jahannum", "assets/images/quizbg/jahannumbg.jpg"),
+    Category("Jannah", "assets/images/quizbg/jannahbg.jpg"),
 ];
