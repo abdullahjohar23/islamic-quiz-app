cd031a3458c4b262fcc8e1e8bc09403b37395a06
Abdullah Johar <jrrabdullah23@gmail.com>
2025-09-01 14:50:56 +0600

    quiz category list page name updated

---
+++ lib/pages/quiz_category_list.dart
@@ -0,0 +1,21 @@
+class Category {
+    final String name;
+    final String imagePath;
+
+    Category(this.name, this.imagePath);
+}
+
+final List<Category> categories = [
+    Category("Allah", "assets/images/quizbg/Allahbg.png"),
+    Category("Qur'an", "assets/images/quizbg/quranbg.png"),
+    Category("Seerah", "assets/images/quizbg/prophetmuhammadbg.png"),
+    Category("Prophets", "assets/images/quizbg/prophetsbg.jpg"),
+    Category("Companions", "assets/images/quizbg/companionsbg.jpg"),
+    Category("Women in Islam", "assets/images/quizbg/womenbg.jpg"),
+    Category("Fiqh", "assets/images/quizbg/fiqhbg.png"),
+    Category("Aqeeda", "assets/images/quizbg/quranbg.png"),
+    Category("Islamic History", "assets/images/quizbg/quranbg.png"),
+    Category("Qiyamah", "assets/images/quizbg/quranbg.png"),
+    Category("Jahannum", "assets/images/quizbg/quranbg.png"),
+    Category("Jannah", "assets/images/quizbg/quranbg.png"),
+];
