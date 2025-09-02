cb048f5aa49af1a9b8f558a5c731e10536b53d18
Abdullah Johar <jrrabdullah23@gmail.com>
2025-09-02 15:42:59 +0600

    topPadding issue solved

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,97 +10,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    late double deviceHeight, deviceWidth;
+    late double deviceHeight, deviceWidth, topPadding;
 
     @override
     Widget build(BuildContext context) {
         deviceWidth = MediaQuery.of(context).size.width;
         deviceHeight = MediaQuery.of(context).size.height;
         
+        // Get reported padding
+        topPadding = MediaQuery.of(context).padding.top;
+
+        // If device reports 0 (like Vivo Y19), fallback to 24 (typical status bar height)
+        if (topPadding == 0) {
+            topPadding = 24; // checks if padding.top is 0, and if yes, forces 24 px manually (because most Android status bars are ~24 px)
+        }
+
         return Scaffold(
-            body: SafeArea(
-                child: Column(
-                    children: [
-                        // Header with profile & points
-                        Padding(
-                            padding: EdgeInsets.symmetric(horizontal: deviceWidth*0.04),
+            body: Column(
+                children: [
+                    // Header with profile & points
+                    Padding(
+                        padding: EdgeInsets.only(
+                            left: deviceWidth*0.04,
+                            right: deviceWidth*0.04,
+                            top: topPadding + 10
+                        ),
+                        
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                
-                                children: [
-                                    Row(
-                                        children: [
-                                            CircleAvatar(
-                                                backgroundColor: Color(0xFF675496),
-                                                child: Icon(Icons.person, color: Colors.white, size: deviceWidth*0.08),
-                                            ),
-                                            
-                                            SizedBox(width: deviceWidth*0.03),
-                                            
-                                            const Text(
-                                                'Abdullah Johar',
-                                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                            ),
-                                        ],
-                                    ),
-
-                                    Row(
-                                        children: [
-                                            Image.asset(
-                                                'assets/images/badges/amethyst.png',
-                                                height: deviceWidth*0.07,
-                                            ),
-
-                                            SizedBox(width: deviceWidth*0.01),
-                                            const Text('120', style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 114, 35, 163), fontWeight: FontWeight.bold)),
-                                        ],
-                                    ),
-                                ],
-                            ),
+                            children: [
+                                Row(
+                                    children: [
+                                        CircleAvatar(
+                                            backgroundColor: Color(0xFF675496),
+                                            child: Icon(Icons.person, color: Colors.white, size: deviceWidth*0.08),
+                                        ),
+                                        
+                                        SizedBox(width: deviceWidth*0.03),
+                                        
+                                        const Text(
+                                            'Abdullah Johar',
+                                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                        ),
+                                    ],
+                                ),
+            
+                                Row(
+                                    children: [
+                                        Image.asset(
+                                            'assets/images/badges/amethyst.png',
+                                            height: deviceWidth*0.07,
+                                        ),
+            
+                                        SizedBox(width: deviceWidth*0.01),
+                                        const Text('120', style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 114, 35, 163), fontWeight: FontWeight.bold)),
+                                    ],
+                                ),
+                            ],
                         ),
-
-                        // Search Bar
-                        Padding(
-                            padding: EdgeInsets.symmetric(horizontal: deviceWidth*0.04, vertical: deviceWidth*0.05),
-                            child: TextField(
-                                decoration: InputDecoration(
-                                    hintText: "Search Quiz",
-                                    prefixIcon: Icon(Icons.search, size: deviceWidth*0.06),
-                                    border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12.0),
-                                    ),
+                    ),
+            
+                    // Search Bar
+                    Padding(
+                        padding: EdgeInsets.symmetric(horizontal: deviceWidth*0.04, vertical: deviceWidth*0.05),
+                        child: TextField(
+                            decoration: InputDecoration(
+                                hintText: "Search Quiz",
+                                prefixIcon: Icon(Icons.search, size: deviceWidth*0.06),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
                                 ),
                             ),
                         ),
-
-                        // Quiz Categories Grid
-                        Expanded(
-                            child: GridView.builder(
-                                padding: EdgeInsets.all(deviceWidth * 0.04),
-                                
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 3,
-                                    crossAxisSpacing: deviceWidth * 0.025,
-                                    mainAxisSpacing: deviceWidth * 0.025,
-                                ),
-                                
-                                itemCount: categories.length,
-                                itemBuilder: (context, index) {
-                                    final category = categories[index];
-
-                                    return QuizCard(
-                                        title: category.name,
-                                        imagePath: category.imagePath,
-                                        onTap: () {
-                                            // navigate to quiz page later
-                                        },
-                                    );
-                                },
+                    ),
+            
+                    // Quiz Categories Grid
+                    Expanded(
+                        child: GridView.builder(
+                            padding: EdgeInsets.all(deviceWidth * 0.04),
+                            
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                crossAxisSpacing: deviceWidth * 0.025,
+                                mainAxisSpacing: deviceWidth * 0.025,
                             ),
+                            
+                            itemCount: categories.length,
+                            itemBuilder: (context, index) {
+                                final category = categories[index];
+            
+                                return QuizCard(
+                                    title: category.name,
+                                    imagePath: category.imagePath,
+                                    onTap: () {
+                                        // navigate to quiz page later
+                                    },
+                                );
+                            },
                         ),
-                    ],
-                ),
+                    ),
+                ],
             ),
         );
     }
