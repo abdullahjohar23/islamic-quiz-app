f2aa529aaa9db584a5e4b3eccbfcea8105f6f200
Abdullah Johar <jrrabdullah23@gmail.com>
2025-09-07 15:45:23 +0600

    designs using media query

--- lib/pages/badge_card_design.dart
+++ lib/pages/badge_card_design.dart
@@ -18,31 +18,34 @@ class BadgeCard extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
+        double deviceWidth = MediaQuery.of(context).size.width;
+        double deviceHeight = MediaQuery.of(context).size.height;
+
+        
         return Container(
-            margin: const EdgeInsets.all(6),
+            padding: EdgeInsets.only(top: deviceHeight*0.01),
             
             decoration: BoxDecoration(
                 border: Border.all(color: Colors.black26),
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(12),
                 color: Colors.white,
             ),
 
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                     Image.asset(
-                        isUnlocked ? badge.imagePath : "assets/images/badges/questionmark.png",
-                        height: 50,
-                        width: 50,
+                        isUnlocked ? badge.imagePath : 'assets/images/badges/questionmark.png',
+                        width: deviceWidth*0.085,
                         fit: BoxFit.contain,
                     ),
                     
-                    const SizedBox(height: 6),
+                    SizedBox(height: deviceHeight*0.008),
                     
                     Text(
                         badge.name,
                         style: TextStyle(
-                            fontSize: 12,
+                            fontSize: deviceWidth*0.035,
                             fontWeight: FontWeight.bold,
                             color: isUnlocked ? badge.color : Colors.grey,
                         ),
